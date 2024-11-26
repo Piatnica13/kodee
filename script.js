f27aee70696e86781be128cf057ced3a3bacbd0b
Piatnica13 <konstantin.gorkovenko13@gmail.com>
2024-11-26 17:51:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let checkbox = document.querySelector("#checkboxx");
+let divSrtelkaMenu = document.querySelector("#strelkaMenuOp");
+let chet = 0;
+checkbox.addEventListener('click', () =>{
+    divSrtelkaMenu.classList.toggle('open');
+})
